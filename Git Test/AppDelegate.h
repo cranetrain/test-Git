4fa88b8b0976ef6cac674ff2da1005fc8801adf7
Kevin Crane <kevin@Kevins-MacBook-Air.local>
2014-10-08 14:04:29 +0300

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Kevin Crane on 10/8/14.
+//  Copyright (c) 2014 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Kevin Crane on 10/8/14.
+//  Copyright (c) 2014 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
